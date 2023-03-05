2e17af4089c3085ae5d970fbe1e580aaee807891
Karolgos <karoolgos@gmail.com>
2023-03-05 17:14:04 +0100

    addedThankYouPageAndBgImg

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const $mainPage = document.getElementById("mainPage");
+const $shoppingCartPage = document.getElementById("shoppingCart");
+const $thankYouPage = document.getElementById("thankYouPage");
+const $buyBtn = document.getElementsByClassName("buyBtn");
+const $backBtn = document.getElementById("backBtn");
+const $confirmBtn = document.getElementById("confirmBtn");
+const $mainPageBtn = document.getElementById("mainPageBtn");
 
+for (let i = 0; i < $buyBtn.length; i++) {
+  $buyBtn[i].addEventListener("click", function () {
+    $mainPage.style.display = "none";
+    $shoppingCartPage.style.display = "flex";
+  });
+}
+
+$backBtn.addEventListener("click", function () {
+  $shoppingCartPage.style.display = "none";
+  $mainPage.style.display = "block";
+});
+
+$confirmBtn.addEventListener("click", function () {
+  $shoppingCartPage.style.display = "none";
+  $thankYouPage.style.display = "block";
+});
+
+$mainPageBtn.addEventListener("click", function () {
+  $thankYouPage.style.display = "none";
+  $mainPage.style.display = "block";
+});
